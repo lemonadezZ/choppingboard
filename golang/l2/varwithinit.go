f3125a63f053b5c82629a822d832c86c28c4abe6
lemonade <lemonade@jingdor.com>
2016-11-10 20:47:01 +0800

    var with shortinit

---
+++ golang/l2/varwithinit.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+var php , java bool = true,false
+
+func main(){
+  var a , b int=1111111 ,11
+  fmt.Println(php,java,a,b)
+}
---
+++ golang/l2/varwithshortinit.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+var php , java bool = true,false
+
+func main(){
+   a , b:=1111111 ,11
+  fmt.Println(php,java,a,b)
+}
