cc6d0748fdc62c3227d58aaf232abc2f4c174c60
lemonade <lemonade@jingdor.com>
2016-11-10 20:12:42 +0800

    go import

---
+++ golang/l2/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+  "fmt"
+  "math"
+  "math/rand"
+)
+
+func main(){
+  fmt.Println("my favorite number is",rand.Intn(10))
+  fmt.Printf("now you have %g problems\n",math.Sqrt(7))
+}
