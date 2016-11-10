f2993b25133104cd7e99042d003cb6b4eb006095
lemonade <lemonade@jingdor.com>
2016-11-10 20:29:15 +0800

    add int swap string swap

--- golang/l2/add.go
+++ golang/l2/add.go
@@ -2,10 +2,15 @@ package main
 
 import "fmt"
 
-func add(int x,int y) int {
+func add(x int,y int) int {
+  return x+y
+}
+
+func add1(x,y int) (int) {
   return x+y
 }
 
 func main(){
-  fmt.println(add(1,2))
+  fmt.Println(add(1,2))
+  fmt.Println(add1(1,2))
 }
