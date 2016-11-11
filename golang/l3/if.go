3ccef0d4e01141afbfdd1ac82a4126ec79d70e6b
lemonade <lemonade@jingdor.com>
2016-11-11 14:19:12 +0800

    add switch

---
+++ golang/l3/if.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main(){
+  var a=10
+  if a=40 ;a>30 {
+    
+  fmt.Println(a)
+  }  
+
+  fmt.Println(a)
+}
---
+++ golang/l3/switch.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main(){
+  var a=10
+  switch  a=40 ;a {
+    case 10:
+  fmt.Println(a)
+      break
+    case 40:
+  fmt.Println(a)
+      break
+    default:  
+  fmt.Println(a)
+    break
+  }  
+
+  fmt.Println(a)
+}
