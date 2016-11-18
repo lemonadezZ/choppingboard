41e14da0447564cd5f4e98851b72bf28aa2ca079
lemondezZ <lemonade@jingdor.com>
2016-11-18 20:49:53 +0800

    method are function

--- golang/l5/method.go
+++ golang/l5/method.go
@@ -14,9 +14,14 @@ func (p person)getage()int{
 	return p.age
 }
 
+func getage1(p person)int{
+	fmt.Println(p.age)
+	return p.age
+}
 
 func main(){
   	p:=person{"jingdor",18}
 	p.getage()
+	getage1(p)
 	fmt.Println(p)
 }
